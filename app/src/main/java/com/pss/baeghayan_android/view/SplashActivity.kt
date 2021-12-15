f87381056bdace997a348283cb7ac749b9cebb7c
ParkSangSun <appleasz1028102@gmail.com>
2021-12-15 09:23:29 +0900

    create : SplashActivity

---
+++ app/src/main/java/com/pss/baeghayan_android/view/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.pss.baeghayan_android.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pss.baeghayan_android.R
+import com.pss.baeghayan_android.base.BaseActivity
+import com.pss.baeghayan_android.databinding.ActivitySplashBinding
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+
+    override fun init() {
+
+    }
+}
