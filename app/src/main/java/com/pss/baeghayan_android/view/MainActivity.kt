3135bd9b1a95693e255880af87f081b7bfa5acb6
ParkSangSun <appleasz1028102@gmail.com>
2021-12-16 17:08:04 +0900

    add : google stt

--- app/src/main/java/com/pss/baeghayan_android/view/MainActivity.kt
+++ app/src/main/java/com/pss/baeghayan_android/view/MainActivity.kt
@@ -1,18 +1,79 @@
 package com.pss.baeghayan_android.view
 
+import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
 import com.pss.baeghayan_android.R
 import com.pss.baeghayan_android.base.BaseActivity
 import com.pss.baeghayan_android.databinding.ActivityMainBinding
 import com.pss.baeghayan_android.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val mainViewModel by viewModels<MainViewModel>()
+    private val REQUEST_CODE = 1
+    private var speechRecognizer: SpeechRecognizer? = null
 
 
     override fun init() {
+        if (Build.VERSION.SDK_INT >= 23)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO), REQUEST_CODE)
 
+        // STTButton 클릭시 onActivityResult를 이용한 startSTT() 함수 실행
+        binding.btn.setOnClickListener { startSTTUseActivityResult() }
+    }
+
+    // onActivityResult 사용한 예제
+    private fun startSTTUseActivityResult() {
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+
+        try {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            );
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
+            startActivityForResult(intent, 100)
+        }catch( e : ActivityNotFoundException)
+        {
+            shortShowToast("녹음할수 있는 어플을 다운 받아주세요")
+           /* val browserIntent = Intent(Intent.ACTION_VIEW,   Uri.parse("https://market.android.com/details?id=APP_PACKAGE_NAME"))
+            startActivity(browserIntent);*/
+        }
+     /*   val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+
+            putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName)
+            //putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.KOREA)
+        }
+
+        startActivityForResult(speechRecognizerIntent, 100)*/
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == 100) {
+            binding.txt.text = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)!![0]
+        }
+    }
+
+    override fun onDestroy() {
+
+        if (speechRecognizer != null) {
+            speechRecognizer!!.stopListening()
+        }
+
+        super.onDestroy()
     }
 }
