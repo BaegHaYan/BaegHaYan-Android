619cbe5dfe52a48d7d16049810a4f99ca6f668fb
ParkSangSun <appleasz1028102@gmail.com>
2021-12-17 10:11:01 +0900

    add : 전면카메라 표시 (사용자 얼굴)

--- app/src/main/java/com/pss/baeghayan_android/view/MainActivity.kt
+++ app/src/main/java/com/pss/baeghayan_android/view/MainActivity.kt
@@ -1,79 +1,178 @@
 package com.pss.baeghayan_android.view
 
 import android.Manifest
-import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.net.Uri
+import android.content.pm.PackageManager
 import android.os.Build
+import android.os.Bundle
+import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import androidx.activity.viewModels
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.pss.baeghayan_android.R
 import com.pss.baeghayan_android.base.BaseActivity
 import com.pss.baeghayan_android.databinding.ActivityMainBinding
-import com.pss.baeghayan_android.viewmodel.MainViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-    private val mainViewModel by viewModels<MainViewModel>()
-    private val REQUEST_CODE = 1
-    private var speechRecognizer: SpeechRecognizer? = null
+    private lateinit var speechRecognizer: SpeechRecognizer
+    private lateinit var recognitionListener: RecognitionListener
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+    private val REQUEST_CODE_PERMISSIONS = 10
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 
     override fun init() {
-        if (Build.VERSION.SDK_INT >= 23)
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO), REQUEST_CODE)
-
-        // STTButton 클릭시 onActivityResult를 이용한 startSTT() 함수 실행
-        binding.btn.setOnClickListener { startSTTUseActivityResult() }
+        binding.activity = this
+        requestPermission()
+        checkPermissions()
+        initVoiceListener()
+        initVoiceIntent()
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    // onActivityResult 사용한 예제
-    private fun startSTTUseActivityResult() {
+    fun clickStartVoiceBtn(view : View){
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
-
-        try {
-            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            intent.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-            );
-            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
-            startActivityForResult(intent, 100)
-        }catch( e : ActivityNotFoundException)
-        {
-            shortShowToast("녹음할수 있는 어플을 다운 받아주세요")
-           /* val browserIntent = Intent(Intent.ACTION_VIEW,   Uri.parse("https://market.android.com/details?id=APP_PACKAGE_NAME"))
-            startActivity(browserIntent);*/
-        }
-     /*   val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
-
-            putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName)
-            //putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.KOREA)
-        }
-
-        startActivityForResult(speechRecognizerIntent, 100)*/
+        speechRecognizer.setRecognitionListener(recognitionListener)
+        speechRecognizer.startListening(intent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (resultCode == Activity.RESULT_OK && requestCode == 100) {
-            binding.txt.text = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)!![0]
+    private fun initVoiceIntent(){
+        var intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
+    }
+    
+    private fun checkPermissions(){
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
         }
     }
 
-    override fun onDestroy() {
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 
-        if (speechRecognizer != null) {
-            speechRecognizer!!.stopListening()
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
         }
+    }
 
-        super.onDestroy()
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e("로그", "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 0)
+        }
+    }
+
+    private fun initVoiceListener() {
+        recognitionListener = object : RecognitionListener {
+            override fun onReadyForSpeech(p0: Bundle?) {
+                shortShowToast("음성인식 시작")
+            }
+
+            override fun onBeginningOfSpeech() {
+
+            }
+
+            override fun onRmsChanged(p0: Float) {
+            }
+
+            override fun onBufferReceived(p0: ByteArray?) {
+            }
+
+            override fun onEndOfSpeech() {
+            }
+
+            override fun onError(p0: Int) {
+                shortShowToast("에러발생")
+            }
+
+            override fun onResults(p0: Bundle?) {
+                val matches: ArrayList<String> =
+                    p0?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
+                for (i in 0 until matches.size) {
+                    binding.tvResult.text = matches[i]
+                }
+
+            }
+
+            override fun onPartialResults(p0: Bundle?) {
+                Log.d("로그", "partialresult : $p0")
+            }
+
+            override fun onEvent(p0: Int, p1: Bundle?) {
+                Log.d("로그", "event : $p0, $p1")
+            }
+
+        }
     }
 }
